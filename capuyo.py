f2fe310137a93bef3414e09239f134d946d8565e
Ryota Yoshikawa <ryota-yoshikawa@cookpad.com>
2018-06-28 19:57:50 +0900

    Fix

--- capuyo.py
+++ capuyo.py
@@ -1,34 +1,48 @@
 import cv2
 import numpy as np
 
-board = cv2.imread('./puyo2.png')
+TEMPLATE_THRESHOLD = 0.6
+BOARD_FILE = './puyo2.png'
 
-templates = {
-    'blue': cv2.imread('templates/b.png'),
-    'purple': cv2.imread('templates/p.png'),
-    'ojama': cv2.imread('templates/o.png'),
-    'red': cv2.imread('templates/r.png'),
-    'green': cv2.imread('templates/g.png'),
+board = cv2.imread(BOARD_FILE)
+result = cv2.imread(BOARD_FILE)
+
+colors = {
+    'red': {
+        'template': cv2.imread('templates/r.png'),
+        'bgr': [0, 0, 255]
+    },
+    'green': {
+        'template': cv2.imread('templates/g.png'),
+        'bgr': [0, 128, 0]
+    },
+    'blue': {
+        'template': cv2.imread('templates/b.png'),
+        'bgr': [255, 0, 0]
+    },
+    'purple': {
+        'template': cv2.imread('templates/p.png'),
+        'bgr': [128, 0, 128]
+    },
+    'ojama': {
+        'template': cv2.imread('templates/o.png'),
+        'bgr': [0, 0, 0]
+    },
 }
 
-detect_color = 'ojama'
+for color, contents in colors.items():
+    template = contents['template']
+    match = cv2.matchTemplate(board, template, cv2.TM_CCOEFF_NORMED)
+    histogram = cv2.calcHist([template], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
 
-match = cv2.matchTemplate(board, templates[detect_color], cv2.TM_CCOEFF_NORMED)
-threshold = 0.35
+    h, w, c = template.shape
+    loc = np.where(match >= TEMPLATE_THRESHOLD)
 
-h, w, c = templates[detect_color].shape
-loc = np.where(match >= threshold)
+    for pt in zip(*loc[::-1]):
+        cropped = board[pt[1]:pt[1]+h, pt[0]:pt[0]+w]
+        cropped_hist = cv2.calcHist([cropped], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
+        d = cv2.compareHist(histogram, cropped_hist, cv2.HISTCMP_CORREL)
+        if d > 0.1:
+            cv2.rectangle(result, pt, (pt[0] + w, pt[1] + h), contents['bgr'], 2)
 
-for pt in zip(*loc[::-1]):
-    cropped = board[pt[1]:pt[1]+h, pt[0]:pt[0]+w]
-    blue, green, red = cropped.mean(0).mean(0)
-    # if blue > red and blue > green:
-    #     cv2.rectangle(board, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-    # if green > blue and green > red:
-    #     cv2.rectangle(board, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-    if blue < 120 and green < 120 and red > 170:
-        cv2.rectangle(board, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-    # cv2.rectangle(board, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-
-cv2.imwrite('result.png', board)
-cv2.imwrite('cropped.png', cropped)
+cv2.imwrite('result.png', result)
